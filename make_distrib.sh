b2e4d9b124c64d99a9ccacec20cefc3dce9234de
smccrory <>
2000-07-22 23:38:29 +0000

    Test commit

--- make_distrib.sh
+++ make_distrib.sh
@@ -10,16 +10,16 @@
 ###############################################################################
 
 # First refresh the docs in the HTML tree
-cp -f CHANGELOG CONTRIBUTING CONTRIBUTORS INSTALL LICENSE README TODO hlmaps.conf.distrib hlmaps.pl htdocs/
+cp -f CHANGELOG CONTRIBUTING CONTRIBUTORS INSTALL LICENSE README TODO hlmaps.conf.distrib hlmaps.pl ../hlmaps-htdocs/
 
 # Now prepare the distribution directory
-rm -rf hlmaps_release
-mkdir hlmaps_release
-cp -f CHANGELOG CONTRIBUTING CONTRIBUTORS INSTALL LICENSE README TODO hlmaps.conf.distrib hlmaps.pl hlmaps_release/
+rm -rf ../hlmaps-release
+mkdir ../hlmaps-release
+cp -f CHANGELOG CONTRIBUTING CONTRIBUTORS INSTALL LICENSE README TODO hlmaps.conf.distrib hlmaps.pl ../hlmaps-release/
 
 # Tar and gzip the release and html packages
-tar cvf hlmaps_release.tar hlmaps_release
-gzip -9 hlmaps_release.tar
-tar cvf hlmaps_htdocs.tar htdocs
-gzip -9 hlmaps_htdocs.tar
+tar cvf ../hlmaps-release.tar ../hlmaps-release
+gzip -9 ../hlmaps-release.tar
+tar cvf ../hlmaps-htdocs.tar ../htdocs
+gzip -9 ../hlmaps-htdocs.tar
 
